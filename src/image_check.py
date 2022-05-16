a3406399e800c3abef1a8eea77799dbe96edfb88
Xingbang Liu <xingbangliu@dhcp-10-105-213-81.wireless.northwestern.private>
2022-05-16 00:35:31 -0500

    added db for imaegs

--- src/image_check.py
+++ src/image_check.py
@@ -1,12 +1,25 @@
 #!/bin/bash
-"""Check if there are any broken images."""
+"""Check if there are any broken images, and index them."""
 
 import os
+import uuid
 from PIL import Image, ImageFile, UnidentifiedImageError
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
+# Declear
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 folder_path = "./img"
 extensions = []
+
+# Create database
+engine = create_engine("sqlite:///imageMeta.db", echo=True)
+meta = MetaData()
+images = Table(
+    "images", meta, Column("id", Integer, primary_key=True), Column("filename", String),
+)
+meta.create_all(engine)
+
+# Checking images
 for filee in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filee)
     print("** Path: {}  **".format(file_path), end="\r", flush=True)
@@ -18,3 +31,9 @@ for filee in os.listdir(folder_path):
         print("Deleted: ", file_path)
     if filee.split(".")[1] not in extensions:
         extensions.append(filee.split(".")[1])
+
+# Indexing images
+for filee in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filee)
+    conn = engine.connect()
+    fin = conn.execute(images.insert().values(filename=file_path))
